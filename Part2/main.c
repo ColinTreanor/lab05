45ad4dff892369134496c29a4fe712477514ff46
ColinTreanor <colintreanor74@gmail.com>
2023-10-13 16:07:40 -0500

    Part 2 Lab 05

--- Part2/main.c
+++ Part2/main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #define s 2
 #define fg 3
 #define td 6
@@ -8,7 +7,7 @@
 #define td2p 8
 
 void score_combos(int score){
-    char *scoringoptions[]= {"td+2", "td + 2fg", "td", "3fg", "s"};
+    char *scoringoptions[]= {"td+2", "td + fg", "td", "3pt fg", "s"};
     int vals[] = {8,7,6,3,2};
 
     for (int td2pt = 0; td2pt <= score/vals[0]; td2pt++){
@@ -16,8 +15,8 @@ void score_combos(int score){
             for (int t = 0; t <= score/vals[2]; t++) {
                 for (int fgl = 0; fgl <= score/vals[3]; fgl++) {
                     for (int sfty = 0; sfty <= score/vals[4]; sfty++) {
-                        if (t+2 * vals[0] + td_fg * vals[1] + t * vals[2] + fgl * vals[3] + sfty * vals[4] == score) {
-                            printf("%d %s, %d %s, %d %s, %d %s, %d %s\n", td2p, scoringoptions[0], tdfg, scoringoptions[1], td, scoringoptions[2], fg, scoringoptions[3], sfty, scoringoptions[4]);
+                        if (td2pt * vals[0] + td_fg * vals[1] + t * vals[2] + fgl * vals[3] + sfty * vals[4] == score) {
+                            printf("%d %s, %d %s, %d %s, %d %s, %d %s\n", td2pt, scoringoptions[0], td_fg, scoringoptions[1], t, scoringoptions[2], fgl, scoringoptions[3], sfty, scoringoptions[4]);
                         }
                     }
                 }
